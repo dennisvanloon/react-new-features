f41af1f0c5e2fb6cdf39f39bd6f55f3d56fc5f3c
Dennis van Loon <>
2021-02-08 17:23:37 +0100

    useReducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
@@ -27,16 +27,31 @@ import reportWebVitals from './reportWebVitals';
 //   )
 // }
 
+const notesReducer = (state, action) => {
+  switch(action.type) {
+    case 'POPULATE_NOTES':
+      return action.notes
+    case 'ADD_NOTE':
+      return [...state, 
+        { title: action.title, body: action.body }
+      ]
+    case 'REMOVE_NOTE':
+      return state.filter((note) => note.title !== action.title)
+    default:
+      return state
+  }
+}
+
 const NoteApp = () => {
-  
-  const [notes, setNotes] = useState([])
+
+  const [notes, dispatch] = useReducer(notesReducer, [])
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
 
   useEffect(() => {
-    const notesData = JSON.parse(localStorage.getItem('notes'))
-    if (notesData) {
-      setNotes(notesData)  
+    const notes = JSON.parse(localStorage.getItem('notes'))
+    if (notes) {
+      dispatch({ type: 'POPULATE_NOTES', notes})
     }
   }, [])
 
@@ -46,16 +61,20 @@ const NoteApp = () => {
 
   const addNote = (e) => {
     e.preventDefault();
-    setNotes([
-      ...notes,
-      { title, body }
-    ]);
+    dispatch ({
+      type: 'ADD_NOTE',
+      title,
+      body
+    })
     setTitle('');
     setBody('');
   }
 
   const removeNote = (title) => {
-    setNotes(notes.filter((note) => note.title !== title));
+    dispatch ({
+      type: 'REMOVE_NOTE',
+      title
+    })
   }
 
   return  (
